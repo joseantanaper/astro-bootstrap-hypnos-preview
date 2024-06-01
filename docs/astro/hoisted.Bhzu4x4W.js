6371ff3eb9f2121a56ea2d952ef5a408bc7b43c6
joseantanaper (Desktop) <joseantanaper@gmail.com>
2024-06-01 14:02:41 +0200

    New deploy

---
+++ docs/astro/hoisted.Bhzu4x4W.js
@@ -0,0 +1,1 @@
+import{g as e}from"./hoisted.B81_FqXz.js";const t=e.getStoredLocale()||"Not detected",o=e.getStoredTheme()||"Not detected";document.getElementById("app-stored-locale").textContent=t;document.getElementById("app-stored-theme").textContent=o;
