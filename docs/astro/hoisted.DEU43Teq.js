a7f43372fc41821f71f1241cdb9c65edd1449bbf
JoseA <joseantanaper@gmail.com>
2024-06-05 21:35:44 +0200

    Deploy!

---
+++ docs/astro/hoisted.DEU43Teq.js
@@ -0,0 +1,1 @@
+import{g as e}from"./hoisted.D0dfWpEi.js";const t=e.getStoredLocale()||"Not detected",o=e.theme.getStoredTheme()||"Not detected";document.getElementById("app-stored-locale").textContent=t;document.getElementById("app-stored-theme").textContent=o;
