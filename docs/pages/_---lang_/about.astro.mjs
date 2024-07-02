2e567f9b2b66a8feb4e1dea06d6bd8183590d29d
JoseA <joseantanaper@gmail.com>
2024-07-02 22:01:39 +0200

    Deploy

--- docs/pages/_---lang_/about.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../../renderers.mjs';
-
-const page = () => import('../../chunks/about_WTDPCJwU.mjs');
-
-export { page };
--- docs/pages/index.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/index_V1iYGZjR.mjs');
-
-export { page };
